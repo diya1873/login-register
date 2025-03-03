b374860dbe5008460cb7bdee57fb72c67d5b6ccd
diya1873 <d.khasawneh@knooz.com>
2025-03-03 11:44:30 +0300

    jj

--- app.js
+++ app.js
@@ -10,4 +10,10 @@ app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes.js'));
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 module.exports = app;
